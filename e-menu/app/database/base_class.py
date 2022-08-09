2c22e7f8011bc6b3ab3b2bffcca804eb594ef638
mateuszmmi <matheuszmmi@gmail.com>
2022-08-09 10:44:48 +0000

    fix: timezone problems

--- e-menu/app/database/base_class.py
+++ e-menu/app/database/base_class.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, DateTime, Integer
-from sqlalchemy import func
+from datetime import datetime
+
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
@@ -10,5 +11,5 @@ class Base:
         return cls.__name__.lower()
 
     id = Column(Integer, primary_key=True, index=True)
-    date_created = Column(DateTime(timezone=True), server_default=func.now())
-    date_updated = Column(DateTime(timezone=True), onupdate=func.now())
+    date_created = Column(DateTime(timezone=True), default=datetime.now)
+    date_updated = Column(DateTime(timezone=True), onupdate=datetime.now)
